d384936f1531e44d7f3697a0f7fa272f5c6fe234
Frankcisco99 <franciscofigueroabecerra@gmail.com>
2023-12-16 23:55:05 -0300

    inicio asistente virtual

--- app.js
+++ app.js
@@ -29,3 +29,35 @@ document.addEventListener('DOMContentLoaded', function () {
         container.style.transition = 'none';
     });
 });
+var chatModal = document.getElementById('chatModal');
+var messageInput = document.getElementById('message-input');
+var chatBody = document.getElementById('chat-body');
+
+document.getElementById('asistente-virtual').addEventListener('click', function () {
+    chatModal.classList.add('visible');
+});
+
+function cerrarChat() {
+    chatModal.classList.remove('visible');
+}
+
+function enviarMensaje() {
+    var mensaje = messageInput.value.trim();
+    if (mensaje !== '') {
+        chatBody.innerHTML += '<p class="sent">' + mensaje + '</p>';
+        messageInput.value = '';
+
+        // Agrega aquí la lógica para la respuesta del asistente virtual si es necesario
+        // Ejemplo de respuesta automática después de 1 segundo
+        setTimeout(function () {
+            chatBody.innerHTML += '<p class="received">¿En qué puedo ayudarte hoy?</p>';
+        }, 1000);
+    }
+}
+
+// Cierra el chat si se hace clic fuera de la ventana modal
+window.addEventListener('click', function (event) {
+    if (event.target === chatModal) {
+        cerrarChat();
+    }
+});
